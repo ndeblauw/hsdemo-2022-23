3ccbc29e3bf332df1336a7c9d3ca324e84e915f0
Nico Deblauwe <nico@bluepundit.eu>
2022-11-09 17:00:56 +0100

    test

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,16 +3,14 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
-use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    public function index() {
-
+    public function index()
+    {
         $users = User::all();
 
+        //hello
         return view('users', compact('users'));
-
     }
-
 }
