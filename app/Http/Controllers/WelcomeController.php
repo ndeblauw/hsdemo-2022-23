9bf5b25159214f16e49ca778dbda9a0374fb423f
Nico Deblauwe <nico@bluepundit.eu>
2022-11-09 11:38:41 +0100

    Get welcome page to show articles

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -2,13 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Article;
 use Illuminate\Http\Request;
 
 class WelcomeController extends Controller
 {
     public function index(Request $request)
     {
-        ray('Hello from Controller');
-        return view('welcome');
+        $articles = Article::all();
+
+        return view('welcome', compact('articles'));
     }
 }
