d84caa346ae2929e0decba497d2e6e3ea4fd68aa
Nico Deblauwe <nico@bluepundit.eu>
2022-11-11 09:43:30 +0100

    increase the number of articles shown on frontpage

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -9,7 +9,7 @@ class WelcomeController extends Controller
 {
     public function __invoke(Request $request)
     {
-        $articles = Article::latest()->take(2)->get();
+        $articles = Article::latest()->take(4)->get();
         $articles_count = Article::count();
 
         return view('welcome', compact('articles', 'articles_count'));
