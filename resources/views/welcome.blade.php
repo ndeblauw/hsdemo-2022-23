cf96e22366014c24186ef48133e773e4851c046b
Nico Deblauwe <nico@bluepundit.eu>
2022-11-11 09:34:06 +0100

    wip

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,6 +1,5 @@
 <x-site-layout title="Welcome Class">
 
-
     <h2>Latest articles (out of {{$articles_count}})</h2>
     @foreach($articles as $article)
         <h2 class="font-bold">{{ $article->title }}</h2>
