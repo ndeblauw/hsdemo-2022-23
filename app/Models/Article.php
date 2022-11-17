1d0ce64295d0f1883f95658260112911954e35cd
Nico Deblauwe <nico@bluepundit.eu>
2022-11-17 19:28:35 +0100

    Adding media to the project for Articles

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -4,10 +4,15 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Spatie\MediaLibrary\HasMedia;
+use Spatie\MediaLibrary\InteractsWithMedia;
+use Spatie\Image\Manipulations;
+use Spatie\MediaLibrary\MediaCollections\Models\Media;
 
-class Article extends Model
+class Article extends Model implements HasMedia
 {
     use HasFactory;
+    use InteractsWithMedia;
 
     //protected $fillable = ['title', 'body'];
     protected $guarded = [];
@@ -20,5 +25,19 @@ class Article extends Model
     public function keywords()
     {
         return $this->belongsToMany(Keyword::class);
+    }
+
+
+    public function registerMediaConversions(Media $media = null): void
+    {
+        $this
+            ->addMediaConversion('thumbnail')
+            ->fit(Manipulations::FIT_CROP, 50, 50)
+            ->nonQueued();
+        $this
+            ->addMediaConversion('header')
+            ->fit(Manipulations::FIT_CROP, 800, 200)
+            ->nonQueued();
+
     }
 }
