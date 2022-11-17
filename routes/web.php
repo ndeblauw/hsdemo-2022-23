0f5c756e61e11fbf2e14b4ee1db831c55b4ca8d5
Nico Deblauwe <nico@bluepundit.eu>
2022-11-17 23:52:12 +0100

    fixing the routes

--- routes/web.php
+++ routes/web.php
@@ -4,16 +4,19 @@ use Illuminate\Support\Facades\Route;
 
 // PUBLIC ROUTES =========================================================
 Route::get('/', \App\Http\Controllers\WelcomeController::class)->name('welcome');
-Route::resource('users', \App\Http\Controllers\UserController::class);
-Route::resource('articles', \App\Http\Controllers\ArticleController::class);
+Route::resource('users', \App\Http\Controllers\UserController::class)->only('index', 'show');
+Route::resource('articles', \App\Http\Controllers\ArticleController::class)->only('index', 'show');
 
 require __DIR__.'/auth.php';
 
 // LOGGED IN ROUTES ======================================================
-Route::middleware(['auth', 'verified'])->group(function () {
-    Route::get('/dashboard', function () {
-        return view('dashboard');
-    })->name('dashboard');
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->name('dashboard');
+
+Route::middleware(['auth', 'verified'])->prefix('home')->name('home.')->group(function () {
+    Route::resource('articles', \App\Http\Controllers\Home\ArticleController::class);
+
 });
 
 // ADMIN ROUTES ===========================================================
