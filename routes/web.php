6b6341add85c1899ff99445942547280aee27ee5
Nico Deblauwe <nico@bluepundit.eu>
2022-11-22 09:59:06 +0100

    fix web.php

--- routes/web.php
+++ routes/web.php
@@ -3,24 +3,21 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+// PUBLIC ROUTES =========================================================
+Route::get('/', \App\Http\Controllers\WelcomeController::class)->name('welcome');
+Route::resource('users', \App\Http\Controllers\UserController::class)->only('index', 'show');
+Route::resource('articles', \App\Http\Controllers\ArticleController::class)->only('index', 'show');
 
-Route::get('/', function () {
-    return view('welcome');
-});
+require __DIR__.'/auth.php';
 
+// LOGGED IN ROUTES ======================================================
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware(['auth', 'verified'])->prefix('home')->name('home.')->group(function () {
+    Route::resource('articles', \App\Http\Controllers\Home\ArticleController::class);
+});
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
@@ -28,4 +25,7 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/auth.php';
+// ADMIN ROUTES ===========================================================
+Route::middleware(['auth', 'isAdmin'])->group(function () {
+    // admin routes come here
+});
