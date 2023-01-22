f251008de211a7c98178c67fc8eb4368b25106e7
Nico Deblauwe <4344869+ndeblauw@users.noreply.github.com>
2023-01-22 15:42:38 +0100

    BUGFIX :bug: Fix the naming of the API routes to avoid conflicts

--- routes/api.php
+++ routes/api.php
@@ -1,18 +1,10 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+// Route for online payments - Mollie
+Route::post('mollie', [\App\Http\Controllers\BuyArticleController::class, 'webhook'])->name('mollie-webhook');
 
 // Routes for our application API - version 1
-Route::middleware(['auth:sanctum'])->prefix('v1')->group( function() {
+Route::middleware(['auth:sanctum'])->prefix('v1')->name('api.v1.')->group( function() {
     Route::apiResource('articles', \App\Http\Controllers\Api\ArticleController::class)
         ->only(['index', 'show', 'store']);
     Route::apiResource('users', \App\Http\Controllers\Api\UserController::class)->only(['index', 'show']);
@@ -23,5 +15,3 @@ Route::middleware(['auth:sanctum'])->prefix('v1')->group( function() {
 
 
 
-// Route for online payments - Mollie
-Route::post('mollie', [\App\Http\Controllers\BuyArticleController::class, 'webhook'])->name('mollie-webhook');
